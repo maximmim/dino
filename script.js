ca1a851b4c559829d4d6b5ae6f86558156a66123
maximmim <93773088+maximmim@users.noreply.github.com>
2022-02-18 10:43:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const cactus1= document.getElementById("cactus1");
+const dinodown = document.getElementById("dinodown")
 
 
 let i = 1;
 
 function jump() {
-    if(dino.classList != "jump"){
+    if(dino.classList != "jump" && dinodown.classList != "jump"){
         dino.classList.add("jump")
-        dinoq.classList.add("jump")
-        dinow.classList.add("jump")
+        dinodown.classList.add("jump")
 
         i = i+1;
         $('#fe').text(i)
     }
     setTimeout(function(){
         dino.classList.remove("jump")
+        dinodown.classList.remove("jump")
     }, 700)
-}
 
+}
+let downn = false;
+function down() {
+    dino.style.display = "none"
+    dinodown.style.display = "block"
+    cactus.style.top = "125px"
+    setTimeout(downof, 1000)
+}
+function downof() {
+    dino.style.display = "block"
+    dinodown.style.display = "none"
+    cactus.style.top = "110px"
+}
+document.addEventListener('keydown', function(event){
+    jump();
+});
 document.addEventListener("keydown" ,  function(event) {
-jump();
+
 })
 let isAlite = setInterval (function(){
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
