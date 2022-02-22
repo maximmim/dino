bd7047d0760ad7731d0516935c81d4d856d2e884
maximmim <93773088+maximmim@users.noreply.github.com>
2022-02-22 17:13:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function logs() {
     document.getElementById("qqe").style.display = "none";
     document.getElementById("log").style.display = "block";
 }
-setTimeout(logs, 9000)
+setTimeout(logs, 7000)
 
   window.onload = function () {
     document.body.classList.add('loaded_hiding');
