c0156fbc96542f9e9ce7989b79a38da6da9e51af
maximmim <93773088+maximmim@users.noreply.github.com>
2022-03-02 13:41:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const dino = document.getElementById("dino");
+const demon = document.getElementById("demon")
 const dinoan1 = document.getElementById("dinow");
 const dinoan2 = document.getElementById("dinoh");
 const cactus = document.getElementById("cactus");
 const doc = document.getElementById("dac");
 const dinodown = document.getElementById("dinodown")
 let ii = 1;
-
+const demondown = document.getElementById("demondown")
+let dem = false
 localStorage.setItem('dat', 1);
 let daw = localStorage.getItem('dat');
 const livee = document.getElementById("live")
@@ -30,9 +32,10 @@ setInterval(updatea, 100)
 
 $("#www").text(live)
 function jump() {
-    if(dino.classList != "jump" && dinodown.classList != "jump"){
+if(dino.classList != "jump" && dinodown.classList != "jump" && demon.classList != "jump"){
         dino.classList.add("jump");
         i++;    
+        demon.classList.add("jump")
         $("#fs").text(localStorage.dat);
         localStorage.dat++;
         $('#fe').text(i);
@@ -40,29 +43,51 @@ function jump() {
         dinoan1.classList.add("jump");
         dinoan2.classList.add("jump");
         dinodown.classList.add("jump");
-        dino.style.display = "block";
-        dinodown.style.display = "none";
+        //dino.style.display = "block";
+        //dinodown.style.display = "none";
         cactus.style.top = "110px";
 
     }
     setTimeout(function(){
         dino.classList.remove("jump")
         dinodown.classList.remove("jump")
+        demon.classList.remove("jump")
     }, 700)
 
 }
 let da = true;
 let downn = false;
 function down() {
-    dino.style.display = "none"
+
+    if (dem == true) {
+        cactus.style.top = "125px"
+        demon.style.display = "none"
+        demondown.style.display = "block"
+        setTimeout(demonof, 1000);
+    }    
+    else {dino.style.display = "none"
     dinodown.style.display = "block"
     cactus.style.top = "125px"
     setTimeout(downof, 1000)
 }
+}
+
 function downof() {
     dino.style.display = "block"
     dinodown.style.display = "none"
     cactus.style.top = "110px"
+}
+function demonof() {
+    demon.style.display = "block"
+    demondown.style.display = "none"
+    cactus.style.top = "110px"
+}
+function dn() {
+    dino.style.display = "none";
+    demon.style.display = "block";
+    dem = true;
+    document.getElementById("qd").style.display = "none"
+    document.getElementById("demod").style.display = "block"
 }
 document.addEventListener('keydown', function(event){
     jump();
@@ -71,9 +96,11 @@ document.addEventListener("keydown" ,  function(event) {
 
 })
 let isAlite = setInterval (function(){
+    
+    let demonTop = parseInt(window.getComputedStyle(demon).getPropertyValue("top"))
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
-
+//Проверка на столкновения с кактусом 
 if (da == true) {
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) 
   { 
@@ -83,18 +110,29 @@ if (da == true) {
    //document.location.replace('reset.html');
    live = live - 1;
   }
+  if (cactusLeft < 50 && cactusLeft > 0 && demonTop >= 140) 
+  { 
+      
+   console.log("-1 Жизнь")   
+   //alert("ТИ ПРОГРАВ, СПІВЧУВАЮ");
+   //document.location.replace('reset.html');
+   live = live - 1;
+  }
 }
-
+//метод проверки жизней 1
 if (live == 2) {
 $("#www").text(live)
 da = false
 setTimeout(daa,3000)
 }
-
+//метод проверки жизек а потом смерть
 if (live == 1) {
     da = false
     setTimeout(daa,4000)
     $("#www").text(live)
+}
+if (i == 100) {
+    dn()
 }
 if (live == 0) {
 
@@ -145,8 +183,14 @@ function teo() {
 }
 function aw() {
 livee.classList.add("love");
-da = false;
-setTimeout(d,8000)
+//da = false;
+setTimeout(w,100)
+}
+function w() {
+    livee.classList.remove("love")
+    livee.style.display = "none"
+    dn()
+    
 }
 function d() {
     da = true;
